0b93499b9acb74e4ddccea6b41c4a59dcc3b8b91
Rainvillegithub <patrick.rainville@rainvillehealth.com>
2025-12-26 13:50:29 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-// This targets the <div id="root"></div> in your index.html
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 }
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
