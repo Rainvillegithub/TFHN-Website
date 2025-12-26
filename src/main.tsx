a8822b469ea5c175f7b79fe2685591032feb8abb
Rainvillegithub <patrick.rainville@rainvillehealth.com>
2025-12-26 14:40:33 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 const rootElement = document.getElementById('root');
-
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  )
+  );
 }
